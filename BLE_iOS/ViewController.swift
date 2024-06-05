174bde22f99c9e43cda223015fc67873fde9abb7
jooeun kim <jay311311@gmail.com>
2024-06-05 10:54:05 -0400

    Initial Commit

---
+++ BLE_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLE_iOS
+//
+//  Created by Jooeun Kim on 2024-06-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
